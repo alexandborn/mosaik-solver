7b3ac6678c9a64a6733e220059baed9376cfe3d9
alex <alexander.born@outlook.com>
2018-11-01 19:14:58 +0100

    Funktionen sauber benannt und sortiert Alte Funktion neighbor() gelöscht. idx, targ glatt gezogen

--- solver.py
+++ solver.py
@@ -6,20 +6,20 @@ class Mosaik:
     def __init__(self, path):
         gamefile = open(path, 'r')
         self.Prob = []
-        self.Targ = []
+        self.Targs = []
         self.Coord = []
-        for idx, line in enumerate(gamefile):
-            row = line.split()
-            row = [e.replace('-', '-1') for e in row]
-            row = [int(e) for e in row]
-            for col, e in enumerate(row):
+        for row, line in enumerate(gamefile):
+            line = line.split()
+            line = [e.replace('-', '-1') for e in line]
+            line = [int(e) for e in line]
+            self.Prob.append(line)
+            for col, e in enumerate(line):
                 if e >= 0:
-                    self.Targ.append(e)
-                    self.Coord.append([idx,col])
-            self.Prob.append(row)
+                    self.Targs.append(e)
+                    self.Coord.append([row,col])
         gamefile.close()
         tuple(self.Prob)
-        tuple(self.Targ)
+        tuple(self.Targs)
         tuple(self.Coord)
 
         # Spielfeld-Größe ermitteln
@@ -30,28 +30,28 @@ class Mosaik:
 
         # Affected-Matrizen erzeugen
         self.Aff = []
-        for idx, targ in enumerate(self.Targ):
+        for targ, val in enumerate(self.Targs):
             self.Aff.append([[0 for n in range(self.gamesize[1])] for k in range(self.gamesize[0])])
             for row_offset in [-1, 0, 1]:
-                if self.Coord[idx][0] + row_offset < 0 or self.Coord[idx][0] + row_offset == self.gamesize[0]:
+                if self.Coord[targ][0] + row_offset < 0 or self.Coord[targ][0] + row_offset == self.gamesize[0]:
                     continue
                 for col_offset in [-1, 0, 1]:
-                    if self.Coord[idx][1] + col_offset < 0 or self.Coord[idx][1] + col_offset == self.gamesize[1]:
+                    if self.Coord[targ][1] + col_offset < 0 or self.Coord[targ][1] + col_offset == self.gamesize[1]:
                         continue
-                    self.Aff[idx][self.Coord[idx][0]+row_offset][self.Coord[idx][1]+col_offset] = 1
+                    self.Aff[targ][self.Coord[targ][0]+row_offset][self.Coord[targ][1]+col_offset] = 1
         self.Aff = np.asarray(self.Aff)
         self.Aff.flags.writeable = False
 
-        # Neighbours finden
+        # Neighbors finden
         self.Neigh = []
-        for idx, targ in enumerate(self.Targ):
+        for targ, val in enumerate(self.Targs):
             self.Neigh.append([])
-            for kdx, neigh in enumerate(self.Targ):
-                if ((kdx is not idx)
-                    and self.Coord[kdx][0] in range(self.Coord[idx][0]-2,self.Coord[idx][0]+3)
-                    and self.Coord[kdx][1] in range(self.Coord[idx][1]-2,self.Coord[idx][1]+3)
+            for kdx, neigh in enumerate(self.Targs):
+                if ((kdx is not targ)
+                    and self.Coord[kdx][0] in range(self.Coord[targ][0]-2,self.Coord[targ][0]+3)
+                    and self.Coord[kdx][1] in range(self.Coord[targ][1]-2,self.Coord[targ][1]+3)
                     ):
-                    self.Neigh[idx].append(kdx)
+                    self.Neigh[targ].append(kdx)
         tuple(self.Neigh)
 
     def reset_reso(self):
@@ -78,116 +78,128 @@ class Mosaik:
                 line += ' '+e
             print(line)
 
-    def _iter_Targ(func):
-        def wrapper(self):
-            for idx, targ in enumerate(self.Targ):
-                if self.targ_is_defined(idx):
-                    continue
-                func(self, idx, targ)
-        return wrapper
 
-    @_iter_Targ
-    def solve_first(self, idx, targ):
-        # Prüfen, ob Umfeld bereits durch Vorgabewert definiert ist
-        if self.Aff[idx].sum() == targ:
-            self.Reso[self.Aff[idx]==1] = 1
-            return
-        # Im Umfeld einer 0 alles streichen
-        elif targ == 0:
-            self.Reso[self.Aff[idx]==1] = 0
 
-    @_iter_Targ
-    def solve_single_targ(self, idx, targ):
-        if self.right_num_colored(idx):
-            self.cross_undefined(idx)
-            return
-        if self.right_num_crossed(idx):
-            self.color_undefined(idx)
+    ### Solver ############################################################
 
-    def count_colored(self, targ_idx):
-        return self.Reso[(self.Aff[targ_idx]==1)&(self.Reso==1)].size
+    ### Count
 
-    def right_num_colored(self, targ_idx):
-        return self.count_colored(targ_idx) == self.Targ[targ_idx]
+    def count_colored(self, idx):
+        return self.Reso[(self.Aff[idx]==1)&(self.Reso==1)].size
 
-    def right_num_crossed(self, targ_idx):
-        return (self.Reso[(self.Aff[targ_idx]==1)&(self.Reso==0)].size ==
-                self.Aff[targ_idx].sum() - self.Targ[targ_idx])
+    def count_to_col(self, idx):
+        return self.Targs[idx] - self.count_colored(idx)
 
-    def color_undefined(self, targ_idx):
-        self.Reso[(self.Aff[targ_idx]==1)&(self.Reso==-1)] = 1
+    def count_own_undef(self, own_idx, other_idx):
+        return  self.Reso[(self.Aff[own_idx]==1)
+                          &(self.Aff[other_idx]==0)
+                          &(self.Reso==-1)].size
 
-    def cross_undefined(self, targ_idx):
-        self.Reso[(self.Aff[targ_idx]==1)&(self.Reso==-1)] = 0
-
-    def neighbors(self, idx, targ):
-        for neigh in self.Neigh[idx]:
-            # Prüfen, ob im eigenen Umfeld von Nachbar nichts undefinert ist
-            if -1 not in self.Reso[(self.Aff[neigh]==1)&(self.Aff[idx]==0)]:
-                # Anzahl der Felder im eigenen Umfeld von target
-                if self.Reso[(self.Aff[idx]==1)
-                        &(self.Aff[neigh]==0)].size == targ - self.Targ[neigh]:
-                    self.Reso[(self.Aff[idx]==1)&(self.Aff[neigh]==0)] = 1
-
-    @_iter_Targ
-    def solve_neighbors(self, idx, targ):
-        def count_to_col(idx):
-            return self.Targ[idx] - self.count_colored(idx)
-
-        def own_undef(own_idx, other_idx):
-            return  self.Reso[(self.Aff[own_idx]==1)
-                              &(self.Aff[other_idx]==0)
+    def com_to_col(self, own_idx, other_idx):
+        com_undef = self.Reso[(self.Aff[own_idx]==1)
+                              &(self.Aff[other_idx]==1)
                               &(self.Reso==-1)].size
+        com_min = max([self.count_to_col(own_idx)
+                       - self.count_own_undef(own_idx,other_idx), 0])
+        com_max = min([self.count_to_col(own_idx), com_undef])
+        return range(com_min, com_max + 1)
 
-        def com_to_col(own_idx, other_idx):
-            com_undef = self.Reso[(self.Aff[own_idx]==1)
-                                  &(self.Aff[other_idx]==1)
-                                  &(self.Reso==-1)].size
-            com_min = max([count_to_col(own_idx) - own_undef(own_idx,other_idx), 0])
-            com_max = min([count_to_col(own_idx), com_undef])
-            return range(com_min, com_max + 1)
-
-        def color_own_undefined(own_idx, other_idx):
-            self.Reso[(self.Aff[own_idx]==1)
-                      &(self.Aff[other_idx]==0)
-                      &(self.Reso==-1)] = 1
-
-        for neigh in self.Neigh[idx]:
-            targ_com_to_col = com_to_col(idx,neigh)
-            neigh_com_to_col = com_to_col(neigh,idx)
-            intersec_to_col = list(set(targ_com_to_col).intersection(neigh_com_to_col))
-            if len(intersec_to_col) == 1:
-                if count_to_col(idx) - intersec_to_col[0] == own_undef(idx,neigh):
-                    color_own_undefined(idx,neigh)
-                elif count_to_col(idx) == intersec_to_col[0]:
-                    self.Reso[(self.Aff[idx]==1)&(self.Aff[neigh]==0)
-                                  &(self.Reso==-1)] = 0
-
-    def targ_is_defined(self,targ_idx):
-        return -1 not in self.Reso[(self.Aff[targ_idx]==1)]
-
-    def count_all_undefined(self):
+    def count_all_undef(self):
         state, counts = np.unique(self.Reso, return_counts=True)
         state_counts = dict(zip(state, counts))
         if -1 in state_counts:
             return state_counts[-1]
         return 0
 
-    def solved(self):
+
+    ### Check
+
+    def is_solved(self):
         return -1 not in self.Reso
+
+    def is_complete(self,idx):
+        return -1 not in self.Reso[(self.Aff[idx]==1)]
+
+    def right_num_colored(self, idx):
+        return self.count_colored(idx) == self.Targs[idx]
+
+    def right_num_crossed(self, idx):
+        return (self.Reso[(self.Aff[idx]==1)&(self.Reso==0)].size ==
+                self.Aff[idx].sum() - self.Targs[idx])
+
+
+    ### Change Reso
+
+    def col_undef(self, idx):
+        self.Reso[(self.Aff[idx]==1)&(self.Reso==-1)] = 1
+
+    def cross_undef(self, idx):
+        self.Reso[(self.Aff[idx]==1)&(self.Reso==-1)] = 0
+
+    def col_own_undef(self, own_idx, other_idx):
+        self.Reso[(self.Aff[own_idx]==1)
+                  &(self.Aff[other_idx]==0)
+                  &(self.Reso==-1)] = 1
+
+    def cross_own_undef(self, own_idx, other_idx):
+        self.Reso[(self.Aff[own_idx]==1)
+                  &(self.Aff[own_idx]==0)
+                  &(self.Reso==-1)] = 0
+
+    ### Steps to solve
+
+    def _iter_Targs(func):
+        def wrapper(self):
+            for targ, val in enumerate(self.Targs):
+                if self.is_complete(targ):
+                    continue
+                func(self, targ, val)
+        return wrapper
+
+    @_iter_Targs
+    def solve_first(self, idx, val):
+        # Prüfen, ob Umfeld bereits durch Vorgabewert definiert ist
+        if self.Aff[idx].sum() == val:
+            self.Reso[self.Aff[idx]==1] = 1
+            return
+        # Im Umfeld einer 0 alles streichen
+        elif val == 0:
+            self.Reso[self.Aff[idx]==1] = 0
+
+    @_iter_Targs
+    def solve_single(self, idx, val):
+        if self.right_num_colored(idx):
+            self.cross_undef(idx)
+            return
+        if self.right_num_crossed(idx):
+            self.col_undef(idx)
+
+    @_iter_Targs
+    def solve_neighbors(self, targ, val):
+        for neigh in self.Neigh[targ]:
+            targ_com_to_col = self.com_to_col(targ,neigh)
+            neigh_com_to_col = self.com_to_col(neigh,targ)
+            intersec_to_col = list(set(targ_com_to_col)
+                                   .intersection(neigh_com_to_col))
+            targ_own_to_col = self.count_to_col(targ) - intersec_to_col[0]
+            if len(intersec_to_col) == 1:
+                if targ_own_to_col == self.count_own_undef(targ,neigh):
+                    self.col_own_undef(targ,neigh)
+                elif self.count_to_col(targ) == intersec_to_col[0]:
+                    self.cross_own_undef(targ,neigh)
 
     def solve(self):
         self.solve_first()
         iterations = 1
         while True:
-            all_undef = self.count_all_undefined()
-            if self.solved():
+            all_undef = self.count_all_undef()
+            if self.is_solved():
                 print('Gelöst nach ', iterations, ' Iterationen.')
                 break
-            self.solve_single_targ()
-            if self.count_all_undefined() == all_undef:
+            self.solve_single()
+            if self.count_all_undef() == all_undef:
                 self.solve_neighbors()
-                if all_undef == self.count_all_undefined():
+                if self.count_all_undef() == all_undef:
                     print('Nach ', iterations, ' Iterationen nicht gelöst.')
                     break
             iterations += 1
